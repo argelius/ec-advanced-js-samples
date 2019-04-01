1fba6226982a9f6c16c007399b3ea531e5bd3a19
Andreas Argelius <andreas@argeli.us>
2019-04-01 18:38:35 +0200

    Update

--- lesson10/scroll-to-top/src/App.js
+++ lesson10/scroll-to-top/src/App.js
@@ -49,7 +49,7 @@ class App extends Component {
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
             
-            <button onClick={() => scrollToTop(200)}>Scroll to top</button>
+            <button onClick={() => scrollToTop(400)}>Scroll to top</button>
           </div>
       </div>
     );
