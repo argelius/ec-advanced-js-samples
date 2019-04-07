9fee4f600a7b0d180061a7c002482d0da868629f
Andreas Argelius <andreas@argeli.us>
2019-04-07 16:31:25 +0200

    Add example

---
+++ lesson11/hooks-use-state/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+import "./App.css";
+
+function App() {
+  const [ color, updateColor ] = useState('red');
+  const newColor = color === 'red' ? 'blue' : 'red';
+
+  return (
+    <p
+      className="App"
+      onClick={() => updateColor(newColor)}
+      style={{
+        color,
+        cursor: 'pointer',
+      }}
+    >
+      Click me!
+    </p>
+  );
+}
+
+export default App;
